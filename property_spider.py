f4bd4610afdcb7dc99c3af3bbec8f9376f524416
Kathryn Li <146765311+JunyingLLL@users.noreply.github.com>
2024-11-04 10:35:54 -0800

    Update property_spider.py
    Add a trace to follow Next buttom

--- property_spider.py
+++ property_spider.py
@@ -19,8 +19,13 @@ class PropertySpider(scrapy.Spider):
         base_url = 'https://www.realtor.com/'
 
         # Use CSS selectors to select links to page with details
-        hrefs = response.css('div[id^="property_id_"] a::attr(href)').getall()
-
-        for href in hrefs:
-            yield scrapy.Request(urljoin(base_url, href), callback = self.parse_detail_page)
+        links = response.css('div[id^="property_id_"] a::attr(href)').getall()
+        for link in linkss:
+            yield scrapy.Request(urljoin(base_url, link), callback = self.parse_detail_page)
+            
+        # Find the "Next" button link and follow it if it exists
+        next_page = response.css('a.next-link::attr(href)').get()
+        if next_page:
+            # If the next page link is found, follow it and call parse() again
+            yield response.follow(next_page, self.parse)
 
