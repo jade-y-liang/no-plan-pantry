94a0b5eec50cdfadd9e2c88d0ce5c128d0396eba
karis-choi-1224 <choikaris@yahoo.com>
2024-11-05 01:47:16 -0800

    Add files via upload

--- property_spider.py
+++ property_spider.py
@@ -20,7 +20,7 @@ class PropertySpider(scrapy.Spider):
 
         # Use CSS selectors to select links to page with details
         links = response.css('div[id^="property_id_"] a::attr(href)').getall()
-        for link in linkss:
+        for link in links:
             yield scrapy.Request(urljoin(base_url, link), callback = self.parse_detail_page)
             
         # Find the "Next" button link and follow it if it exists
@@ -35,4 +35,19 @@ class PropertySpider(scrapy.Spider):
 #        for url in next_page:
 #            # If the next page link is found, follow it and call parse() again
 #            yield response.follow(url, self.parse)
+    def parse_detail_page(self, response):
+        price = response.css('span.base__StyledType-rui__sc-108xfm0-0.mQZci::text').getall()[0]
+        address = response.css('h1.sc-79f365fd-3.bUJjRW::text').get()
+        beds = response.css('li[data-testid="property-meta-beds"] span::text').get()
+        baths = response.css('li[data-testid="property-meta-baths"] span::text').get()
+        sqft = response.css('li[data-testid="property-meta-sqft"] span::text').getall()[0]
+        lot_size =  response.css('span[data-testid="meta-value"]::text').getall()[3]
 
+        yield {
+            'price': price,
+            'address': address,
+            'beds': beds,
+            'baths': baths,
+            'sqft': sqft,
+            'lot_size': lot_size,
+        }
